6af42c0a654c6dd4d0fc52b7a2ef0488bf22cb72
Dave Decker <dave.decker@gmail.com>
2017-01-04 21:23:44 -0600

    initial commit

---
+++ simplej/settings.py
@@ -0,0 +1,38 @@
+import inspect, os
+import imp
+from simple_jupyter_workflow.constants import SOURCES
+
+
+# git repo for project
+git_origin = 'https://github.com/dfd/example_project.git'
+
+# specify source of image
+# choices are DOCKERFILE, DOCKERHUB, URL, GIT, LOCAL_IMAGE
+source = SOURCES.DOCKERHUB
+#source = SOURCES.GIT
+
+if source == SOURCES.DOCKERHUB:
+    #name of image from Docker Hub
+    image_name = 'eipdev/alpine-jupyter-notebook'
+    image_tag = 'latest'
+
+
+if source == SOURCES.DOCKERFILE:
+    pass
+
+
+if source == SOURCES.URL:
+   url = 'https://gist.githubusercontent.com/dfd/ab87a3d7e232d271a02dddea71af416b/raw/0bc9a001386ee1c555c0b7bc3d90a2266206eab5/Dockerfile' 
+
+
+if source == SOURCES.GIT:
+    git_url = 'https://github.com/CognitiveScale/alpine-jupyter'
+    df_name = 'Dockerfile'
+
+if source == SOURCES.LOCAL_IMAGE:
+   image_id =  '15df50b84f9e'
+
+
+# define directory in container to bind to local notebooks directory
+#dir_to_mount = '/home/jovyan/.jupyter/' # for Dockerfile
+dir_to_mount = '/opt/notebook/' # for 'eipdev/alpine-jupyter-notebook
